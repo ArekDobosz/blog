cd7b49858830694ef7a17eae397cffff43e87a09
Arkadiusz Dobosz <doboszarkadiusz01@gmail.com>
2017-07-06 13:31:49 +0200

    Before avatar file upload

--- src/AutoSerwisBundle/Entity/User.php
+++ src/AutoSerwisBundle/Entity/User.php
@@ -5,12 +5,20 @@ namespace AutoSerwisBundle\Entity;
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 /**
  * @ORM\Entity
  * @ORM\Table(name="fos_user")
+ * @ORM\HasLifecycleCallbacks()
  */
 class User extends BaseUser
 {
+    const UPLOAD_DIR = 'uploads/avatars/';
+    const DEFAULT_AVATAR = 'default.png';
+    
+    
     /**
      * @ORM\Id
      * @ORM\Column(type="integer")
@@ -27,8 +35,33 @@ class User extends BaseUser
      * @ORM\Column(type="string", length=100, nullable=true)
      */
     protected $lastName;
+    
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updateDate;
 
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $avatar;
+    
+    /**
+     * @var UploadedFile
+     * 
+     * @Assert\Image(
+     *      minWidth = 50,
+     *      maxWidth = 150,
+     *      minHeight = 50,
+     *      maxHeight = 150,
+     *      maxSize = "1M",
+     * )
+     */
+    private $avatarFile;
 
+    private $avatarTemp;
+
+    
     public function __construct()
     {
         parent::__construct();
@@ -44,7 +77,7 @@ class User extends BaseUser
     public function setFirstName($firstName)
     {
         $this->firstName = $firstName;
-
+        
         return $this;
     }
 
@@ -80,5 +113,104 @@ class User extends BaseUser
     public function getLastName()
     {
         return $this->lastName;
+    }
+
+    public function getAvatarFile() {
+        return $this->avatarFile;
+    }
+
+    public function setAvatarFile(UploadedFile $avatarFile = null) {
+        $this->avatarFile = $avatarFile;
+        $this->updateDate = new \DateTime();
+        return $this;
+    }
+    
+    /**
+     * Set avatar
+     *
+     * @param string $avatar
+     *
+     * @return User
+     */
+    public function setAvatar($avatar)
+    {        
+        $this->avatar = $avatar;
+
+        return $this;
+    }
+
+    /**
+     * Get avatar
+     *
+     * @return string
+     */
+    public function getAvatar()
+    {
+        if($this->avatar == null){
+            return User::UPLOAD_DIR.USER::DEFAULT_AVATAR; // konkatenacja zwróci uploads/avatars/default-avatar.jpg
+        }
+        return User::UPLOAD_DIR.$this->avatar;
+    }
+    
+
+    /**
+     * Set updateDate
+     *
+     * @param \DateTime $updateDate
+     *
+     * @return User
+     */
+    public function setUpdateDate($updateDate)
+    {
+        $this->updateDate = $updateDate;
+
+        return $this;
+    }
+
+    /**
+     * Get updateDate
+     *
+     * @return \DateTime
+     */
+    public function getUpdateDate()
+    {
+        return $this->updateDate;
+    }
+    
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function preSave() {
+        $this->avatar = 'avatar2.jpg';
+        if(null !== $this->getAvatarFile()){
+            
+            if(null !== $this->avatar){
+                $this->avatarTemp = $this->avatar;
+            }
+            $avatarName = sha1(uniqid(null, true));
+            $this->avatar = $avatarName.'.'.$this->getAvatarFile()->guessExtension();
+        }       
+    }
+    
+    /**
+     * @ORM\PostPersist
+     * @ORM\PostUpdate
+     */
+    public function postSave(){
+        if(null !== $this->getAvatarFile()){
+            
+            $this->getAvatarFile()->move($this->getUploadRootDir(), $this->avatar);
+            unset($this->avatarFile);
+            
+            if(null !== $this->avatarTemp){
+                unlink($this->getUploadRootDir().$this->avatarTemp);
+                unset($this->avatarTemp);
+            }
+        }
+    }
+    
+    public function getUploadedRootDir(){
+        return __DIR__.'/../../../web/'.User::UPLOAD_DIR;
     }
 }
