338e8738a1aa9e85d2511b341b3cb301a62b1247
Arkadiusz Dobosz <doboszarkadiusz01@gmail.com>
2017-07-31 21:01:38 +0200

    Sending email added

--- src/AutoSerwisBundle/Controller/ContactController.php
+++ src/AutoSerwisBundle/Controller/ContactController.php
@@ -10,15 +10,46 @@ use AutoSerwisBundle\Form\ContactType;
 
 class ContactController extends Controller{
     
+    private $blogEmail = 'botblackd988@gmail.com';
+    
     /**
      * @Route(
      *      "/",
      *      name="blog_contact"
      * )
      */
-    public function showFormAction() {
+    public function showFormAction(Request $request) {
         
         $form = $this->createForm(ContactType::class);
+        
+        if($request->isMethod('POST')){
+            $form->handleRequest($request);
+            if($form->isValid() && $form->isSubmitted()){
+                
+                $name = $form->get('name')->getData();
+                $email = $form->get('email')->getData();
+                $msg = $form->get('msg')->getData();
+                
+                $message = (new \Swift_Message('Pytanie z bloga'))
+                        ->setFrom($email)
+                        ->setTo($this->blogEmail)
+                        ->setBody(
+                            $this->renderView(
+                                'AutoSerwisBundle:Email:email.html.twig',
+                                array(
+                                    'name' => $name,
+                                    'msg' => $msg
+                                )    
+                            ),
+                            'text/html'
+                        );
+                
+                $this->get('mailer')->send($message);
+                
+                $this->get('session')->getFlashBag()->add('success', 'Wiadomość została wysłana.');
+                return $this->redirectToRoute('blog_contact');
+            }
+        }
         
         return $this->render('AutoSerwisBundle:Contact:showForm.html.twig', array(
             'form' => $form->createView()
--- src/AutoSerwisBundle/Form/ContactType.php
+++ src/AutoSerwisBundle/Form/ContactType.php
@@ -16,14 +16,26 @@ class ContactType extends AbstractType {
         $builder 
                 ->add('name', \Symfony\Component\Form\Extension\Core\Type\TextType::class, array(
                     'label' => 'Imię',
+                    'required' => true,
+                    'constraints' => array(
+                        new \Symfony\Component\Validator\Constraints\Length(array('min' => 3, 'max' => 25))
+                    ),
                     'mapped' => false
                 ))
                 ->add('email', \Symfony\Component\Form\Extension\Core\Type\EmailType::class, array(
                     'label' => 'E-mail',
+                    'required' => true,
+                    'constraints' => array(
+                        new \Symfony\Component\Validator\Constraints\Email()
+                    ),
                     'mapped' => false
                 ))
                 ->add('msg', \Symfony\Component\Form\Extension\Core\Type\TextareaType::class, array(
                     'label' => 'Treść wiadomości',
+                    'required' => true,
+                    'constraints' => array(
+                        new \Symfony\Component\Validator\Constraints\Length(array('min' => 3, 'max' => 500))
+                    ),
                     'mapped' => false
                 ))
                 ->add('submit', \Symfony\Component\Form\Extension\Core\Type\SubmitType::class, array(
